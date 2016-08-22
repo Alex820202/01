0e429916f4abf039d0a3614525bcd26c2ad76d3f
Alex820202 <ads2004@bk.ru>
2016-08-22 17:30:00 +0700

    Добавлены подстановки

--- App/Db.php
+++ App/Db.php
@@ -3,18 +3,20 @@ namespace App;
 
 class Db {
 	protected $dbh;
+	public $param;
 	
 	public function __construct() {
 		$this -> dbh = new \PDO('mysql:host=127.0.0.1;dbname=test', 'root', '') ;
+		
 	}
 	
-	public function execute($sql){
+	public function execute($sql, $param=[]){
 		$sth = $this->dbh->prepare($sql);
 		$res = $sth->execute();
 		return $res;
 	}
 	
-	public function query($sql, $class){
+	public function query($sql, $class, $param=[]){
 		$sth = $this->dbh->prepare($sql);
 		$res = $sth->execute();
 		if (false !==$res){
